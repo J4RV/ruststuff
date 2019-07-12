10486328572f2b6b503b6a1712db9dcc9250ef77
Jarv <kira-8@hotmail.com>
2019-07-12 23:35:28 +0200

    Improvements and added start.bat thanks /u/christophe_biocca and /u/steveklabnik1

--- shabreaker/src/main.rs
+++ shabreaker/src/main.rs
@@ -1,51 +1,57 @@
+extern crate crossbeam;
 extern crate crypto;
+
+use std::time::Instant;
+
+use crypto::mac::Mac;
 
 use self::crypto::hmac::Hmac;
 use self::crypto::sha1::Sha1;
-use crypto::mac::Mac;
-use std::thread;
-use std::time::Instant;
 
 fn main() {
-    let digest = Sha1::new();
-    let msg = "341567891 487654 500".as_bytes();
     let mac = hex::decode("f3c2ae334dc98a387601c85ef83c77360943023a").unwrap();
-    let mut thread_joins = Vec::new();
 
-    let now = Instant::now();
-    for cpu in 0..num_cpus::get() {
-        let mac = mac.clone();
-        let join = thread::spawn(move || {
-            for i in ((cpu as u8)..=255).step_by(num_cpus::get()) {
-                if let Some(valid_key) = bruteforce_block(i, msg, &mac, &digest) {
-                    println!("Key: {:x?}", valid_key);
-                    println!("Done in {} seconds", now.elapsed().as_secs());
-                    std::process::exit(0);
+    let thread_res = crossbeam::scope(|s| {
+        let msg = "341567891 487654 500".as_bytes();
+        let mac = &mac;
+        let digest = Sha1::new();
+        let now = Instant::now();
+        for cpu in 0..num_cpus::get() {
+            s.spawn(move |_| {
+                for i in ((cpu as u8)..=255).step_by(num_cpus::get()) {
+                    if let Some(valid_key) = bruteforce_block(i, msg, mac, &digest) {
+                        println!("Done in {} seconds", now.elapsed().as_secs());
+                        println!("Key: {:x?} for msg '{}' and mac '{}'", valid_key, std::str::from_utf8(msg).unwrap(), hex::encode(mac));
+                        std::process::exit(0);
+                    }
                 }
-            }
-        });
-        thread_joins.push(join);
-    }
+            });
+        }
+    });
 
-    if let Err(e) = thread_joins.into_iter().try_for_each(|h| h.join()) {
-        println!("Error while joining thread: {:x?}", e);
-        std::process::exit(-2);
+    if let Err(e) = thread_res {
+        println!("Error in thread result: {:x?}", e);
+        std::process::exit(2);
     }
 
     // All threads joined. None found the key.
     println!("Valid key not found!");
-    std::process::exit(-1);
+    std::process::exit(1);
 }
 
 fn bruteforce_block(i: u8, msg: &[u8], mac: &[u8], digest: &Sha1) -> Option<[u8; 4]> {
+    let mut key = [i, 0, 0, 0];
     for x in 0..=255 {
         for y in 0..=255 {
             for z in 0..=255 {
-                if valid_mac(msg, mac, &[i, x, y, z], &digest) {
-                    return Some([i, x, y, z]);
+                if valid_mac(msg, mac, &key, digest) {
+                    return Some(key);
                 }
+                key[3] = z
             }
+            key[2] = y
         }
+        key[1] = x
     }
     return None;
 }
---
+++ shabreaker/start.bat
@@ -0,0 +1,1 @@
+start "SHA Breaker" /wait /b /realtime "./target/release/shabreaker.exe" >> result.txt
